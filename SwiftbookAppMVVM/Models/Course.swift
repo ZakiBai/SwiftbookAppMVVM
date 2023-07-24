0dbe587e8d8f86432ddfb5595e65710f50238a2f
Zaki <menza.zm@gmail.com>
2023-07-24 17:09:17 +0600

    MVVM architecture added

---
+++ SwiftbookAppMVVM/Models/Course.swift
@@ -0,0 +1,14 @@
+//
+//  Course.swift
+//  SwiftbookAppMVVM
+//
+//  Created by Zaki on 24.07.2023.
+//
+
+import Foundation
+
+struct Course: Decodable {
+    let name: String
+    let imageURL: URL
+    let numberOf
+}
---
+++ SwiftbookAppMVVM/Services/ImageManager.swift
@@ -0,0 +1,20 @@
+//
+//  ImageManager.swift
+//  SwiftbookAppMVVM
+//
+//  Created by Zaki on 24.07.2023.
+//
+
+import Foundation
+
+class ImageManager {
+    static let shared = ImageManager()
+    
+    private init() {}
+    
+    func fetchImageData(from url: URL?) -> Data? {
+        guard let url = url else { return nil }
+        guard let imageData = try? Data(contentsOf: url) else { return nil }
+        return imageData
+    }
+}
